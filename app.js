24e139a0e6679d10cfc9b3c8f8ba95985f39b332
jhonkw9w8e <184763080+juankm08-a11y@users.noreply.github.com>
2025-12-24 17:06:42 -0500

    añadiendo endpoint frontend

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:3000",
-      "https://login-frontend-eight.vercel.app/",
+      "https://login-frontend-eight.vercel.app",
     ],
   })
 );
